4763c721a3d4c2023f24b9ffa9286b09728e235d
Marci <hajdumarcel@gmail.com>
2021-04-07 15:05:15 +0200

    started implementing game logic, created changePlayer function and the gameArray

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+let gameArray = ['','','','','','','','',''];
+let player = 'O';
+
+function changePlayer () {
+  if(player === 'X') {
+    player = 'O'
+  } else {
+    player = 'X';
+  }
+};
+
 document.addEventListener('click', (e) => {
   console.log(e.target.dataset.number);
 })
